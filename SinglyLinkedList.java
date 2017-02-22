78a006d1484aa6e0b03cafc0112cb16e16eaa7b8
Daniel Vilinsky <dvilinsk@brandeis.edu>
2017-02-22 00:57:45 -0500

    Add remove and toString method for SinglyLinkedList class

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -37,12 +37,70 @@ public class SinglyLinkedList<E> {
 			tail.next = toInsert;
 			tail = toInsert;
 		}
+		size++;
 	}
 	
+	/** Removes the node containing "data" from the list 
+	*  @param data you want to remove from the list 
+	*  Running time: Since we have to find where (if anywhere) in the list the data is, 
+	*  the running time is O(n). However, this makes a call to .equals(). Depending on how that is 
+	*  implemented, the running time could be worse. 
+	*/
+	public void remove(E data) {
+		if (head != null && head.data.equals(data)) {
+			head = head.next;
+		} else {
+			SinglyLinkedNode<E> toRemove = this.findPrevious(data);
+			if (toRemove == null) {
+				return;
+			} 
+			toRemove.next = toRemove.next.next;
+		}
+		this.size--;
+	}
+	
+	//Helper method for remove. Returns the node previous to the one containing the data, if it exists.
+	//Retuns null if it doesn't.
+	private SinglyLinkedNode<E> findPrevious(E data) {
+		SinglyLinkedNode<E> current = head;
+		while (current != null) {
+			if (current.next != null && current.next.data.equals(data)) {
+				return current;
+			}
+			current = current.next;
+		}
+		return current;
+	}
+
 	/** @return True if list is empty, false otherwise 
 	*   Running time: O(1)
 	*/
 	public boolean isEmpty() {
 		return this.head == null && this.tail == null;
+	}
+	
+	/** @return The size of the list 
+	*   Running time: O(1)
+	*/
+	public int size() {
+		return this.size;
+	}
+	
+	/** Creates a string representation of the form [elem1, elem2...elemN], where elem1 is
+	*   at the head, elemN at the tail.
+	*   @return string representation of the list
+	*   Running time: O(n^2), if string concatenation is O(n)
+	*/
+	public String toString() {
+		SinglyLinkedNode<E> current = head;
+		if (current == null) { //list is empty
+			return "[]";
+		}
+		String s = "[" + current.data;
+		while (current.next != null) {
+			current = current.next;
+			s += ", " + current.data;
+		}
+		return s + "]";
 	}
 }
