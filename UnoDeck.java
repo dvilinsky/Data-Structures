85f3bd81cc0e000e5cac9f6a5b51cb5b6ebabdcf
Daniel Vilinsky <dvilinsk@brandeis.edu>
2017-02-24 03:13:10 -0500

    Write constructor, getLastDiscarded, drawCard, and discard method for UnoDeck class

--- UnoDeck.java
+++ UnoDeck.java
@@ -7,41 +7,82 @@
 import java.util.*;
 public class UnoDeck {
 	private static final String[] REGULAR_COLORS = {"red", "yellow", "blue", "green"};
-	//uncomment once you make your deck, then add all the cards in the constructor to your deck
-	//private SinglyLinkedList<UnoCard> deck; // initialize this in your constructor
-	//private SinglyLinkedList<UnoCard> discard; // initialize this in your constructor
+	private SinglyLinkedList<UnoCard> deck; //deck from which players draw cards 
+	private SinglyLinkedList<UnoCard> discard; // discard pile
 	private UnoCard lastDiscarded;
 	
-	//http://play-k.kaserver5.org/Uno.html
-	// There are 108 cards in a Uno deck. 
-	// There are four suits, Red, Green, Yellow and Blue, 
-	// each consisting of one 0 card, two 1 cards, two 2s, 3s, 4s, 5s, 6s, 7s, 8s and 9s; 
-	// two Draw Two cards; two Skip cards; and two Reverse cards. 
-	// In addition there are four Wild cards and four Wild Draw Four cards.
-
+	/** http://play-k.kaserver5.org/Uno.html
+	 *  There are 108 cards in a Uno deck. 
+	 *  There are four suits, Red, Green, Yellow and Blue, 
+	 *  each consisting of one 0 card, two 1 cards, two 2s, 3s, 4s, 5s, 6s, 7s, 8s and 9s; 
+   	 *  two Draw Two cards; two Skip cards; and two Reverse cards. 
+	 *  In addition there are four Wild cards and four Wild Draw Four cards.
+	 *  This constructor initializes our deck to conform to that standard. The deck is also randomly ordered
+	 *  Running time: Who knows?
+	 */
 	public UnoDeck(){
-		// Initialized as having all 108 cards, as described above
-		
+		this.deck = new SinglyLinkedList<UnoCard>();
+		this.discard = new SinglyLinkedList<UnoCard>();
 		for (String color : REGULAR_COLORS){
-			new UnoCard(color, 0); // add one of your color in zero
+			deck.randomInsert(new UnoCard(color, 0)); // add one of your color in zero
 			for (int i = 0; i<2; i++){
 				// add numbers 1-9
 				for (int cardNumber = 1; cardNumber<=9; cardNumber++){
-					new UnoCard(color, i) // Add this to deck
+					deck.randomInsert(new UnoCard(color, cardNumber)); 
 				}
 				// add 2 of each of the special card for that color
-				new UnoCard(color, true, false, false); // add to deck
-				new UnoCard(color, false, true, false); // add to deck
-				new UnoCard(color, false, false, true); // add to deck
+				deck.randomInsert(new UnoCard(color, true, false, false)); 
+				deck.randomInsert(new UnoCard(color, false, true, false));
+				deck.randomInsert(new UnoCard(color, false, false, true)); 
 			}
 			
 		}
 		// add 4 wild cards, and 4 draw 4 wild cards
 		for (int i = 0; i<4; i++){
-			new UnoCard(false); // add to deck
-			new UnoCard(true); // add to deck
+			deck.randomInsert(new UnoCard(false)); 
+			deck.randomInsert(new UnoCard(true));
 		}
 	}
-
-
+	
+	/** Adds the card c to a random place in the discard pile 
+	*   Running time: O(1)
+    *   @param the card to discard 
+	*   @exception IllegalArgumentException if the card is invalid
+	*/
+	public void discardCard(UnoCard c) {
+		if (!c.canBePlacedOn()) {
+			throw new IllegalArgumentException("Error: This card can't be placed on the deck");
+		}
+		discard.randomInsert(c);
+		this.lastDiscarded = c;
+	}
+	
+	/** Gets the most recently discarded card. Since I've written amazing self-documenting code,
+	*   that was evident from the method signature 
+	*   Running time: O(1)
+	*   @return top of the discard pile 
+	*/
+	public UnoCard getLastDiscarded() {
+		return this.lastDiscarded;
+	}
+	
+	/** Draws a card from the top of the deck. If the deck is empty, it moves all the cards from 
+	*   the discard pile into the deck. 
+	*   Running time: O(n)if the deck is empty, where n is the size of the discard pile. I claim that 
+	*   it's otherwise O(1). removeAt(index) is technically an O(n) method. However, when we pass it 
+	*   the index 0, the for loop in the nodeAt(index) helper method it uses never runs. So, it only 
+	*   ends up doing a bunch of constant time operations. 
+	*   @return: the UnoCard at the top of the deck 
+	*/
+	public UnoCard drawCard() {
+		if (this.deck.isEmpty()) {
+			Iterator<UnoCard> i = this.discard.iterator();
+			while (i.hasNext()) {
+				UnoCard uc = i.next();
+				this.deck.regularInsert(uc);
+			}
+			this.deck.clear();
+		}
+		return this.deck.removeAt(0);
+	}
 }
